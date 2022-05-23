f127e10cb9c87010571116fa3967cc22056c75b6
Kasi Acosta <cacosta@megared.int>
2022-05-23 20:39:21 -0300

    aplicando libreria

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+//Declaraciones
 let divClases = document.getElementById("clases")
 let formulario = document.getElementById('form')
 let divConfirm = document.getElementById("confirm")
 let nuevaSusc = []
+let btnConfirm = document.querySelector('#btnConfirm')
 
-
-
+// Obj
 class Actividades {
   constructor(id, nombre, duracion, frecuenciaSemanal, precio) {
   this.id = id 
@@ -17,23 +18,7 @@ const actividad1 = new Actividades("1", "crossfit", "libre", "libre", "2500")
 const actividad2 = new Actividades("2", "zumba", "60min", "3 veces por semana", "2000" )
 const actividad3 = new Actividades("3", "localizada", "60min", "3 veces por semana", "2300" )
 const actividad4 = new Actividades("4", "sala de aparatos", "libre", "libre", "1900" )
-
 let actividad = [actividad1, actividad2, actividad3, actividad4]
-
-actividad.forEach(clases => {
-  divClases.innerHTML +=`
-  
-  <div class="card clas row conteiner" style="width: 18rem;">
-  
-  <div class="card-body">
-    <h5 class="card-title">Nombre: ${clases.nombre}</h5>
-    <p> Duracion: ${clases.duracion} </p>
-    <p> Frecuencia Semanal: ${clases.frecuenciaSemanal} </p>
-    <p> Precio: $${clases.precio} </p>
-  </div>
-</div>
-  `
-})
 
 class Suscripcion{
   constructor(nombre, apellido, actividad){
@@ -43,6 +28,7 @@ class Suscripcion{
   }
 }
 
+//eventos
 formulario.addEventListener('submit', (event) =>{
   event.preventDefault()
   let datosInsc = new FormData(event.target)
@@ -53,27 +39,49 @@ formulario.addEventListener('submit', (event) =>{
   
   formulario.reset()
 })
-if(localStorage.getItem('NuevaSusc')){
-  nuevaSusc = JSON.parse(localStorage.getItem('NuevaSusc'))
-} else{
-  localStorage.setItem('NuevaSusc', JSON.stringify(nuevaSusc))
-}
+//Op. ternario
+localStorage.getItem('NuevaSusc')?nuevaSusc = JSON.parse(localStorage.getItem('NuevaSusc')):localStorage.setItem('NuevaSusc', JSON.stringify(nuevaSusc))
 
+//Funciones (se aplico desestruc.)
+actividad.forEach(clases => {
+  const {id, nombre, duracion, frecuenciaSemanal, precio} = clases
+  divClases.innerHTML +=`
+  
+  <div id=${id} class="card-body clas">
+    <h5 class="card-title">Nombre: ${nombre}</h5>
+    <p> Duracion: ${duracion} </p>
+    <p> Frecuencia Semanal: ${frecuenciaSemanal} </p>
+    <p> Precio: $${precio} </p>
+  </div>
+  `
+})
 
+function confirmacion() {
 nuevaSusc.forEach(confirm => {
+  const {nombre, apellido, actividad} = confirm
   divConfirm.innerHTML +=`
   
   <div class="card clasConfirm" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Suscripcion Confirmada</h5>
-    <p> Duracion: ${confirm.nombre} </p>
-    <p> Duracion: ${confirm.apellido} </p>
-    <p> Se suscribio a : ${confirm.actividad} </p>
+    <p> Nombre: ${nombre} </p> 
+    <p> Apellido: ${apellido} </p>
+    <p> Se suscribio a : ${actividad} </p>
   </div>
 </div>
   `
 })
+}
+btnConfirm.addEventListener('click', () => {
+  Swal.fire(
+    'Bienvenid@',
+    'Suscripcion completada',
+    'success'
+  )
+})
 
-
+confirmacion();
+//ejemplo Spread
+console.log("Listado completo de actividades", ...actividad, )
 
 
